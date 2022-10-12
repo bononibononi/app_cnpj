977f3ecef7d2e1d79f20a451e3a0269449f4d71c
bononibononi <bononi2003@yahoo.com.br>
2022-10-12 14:58:38 -0300

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import { CnpjComponent } from 
 
-const routes: Routes = [];
+//const routes: Routes = [];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+
+//@NgModule({
+  //imports: [RouterModule.forRoot(routes)],
+  //exports: [RouterModule]
+//})
+//export class AppRoutingModule { }
